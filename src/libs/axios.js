ca883c493096053c3556eb21380b69bc22e0da26
Shri <shrivatsa709@gmail.com>
2026-07-02 12:36:17 +0545

    axios fix

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+
+export const api = axios.create({
+  baseURL: baseURL,
   withCredentials: true,
 });
