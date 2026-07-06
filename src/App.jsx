eb30b8928f0f84304e0a1f64b1bccaebe30a9748
Hrista39 <thapahrista01@gmail.com>
2026-07-06 11:46:55 +0545

    feat: signup integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
 
-const App = () => {
-  return <div></div>;
-};
+import HomePage from "./pages/HomePage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
 
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage /> } />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+}
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+       HopePage
+    </div>
+  )
+}
+
+export default HomePage
