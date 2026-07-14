b04ea310ee97a314433f1ce66edcbe42caec0e27
Shri <shrivatsa709@gmail.com>
2026-07-14 12:54:13 +0545

    added index

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 import { store } from "./stores/store.js";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
