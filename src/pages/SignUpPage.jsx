70c7f3ad4bb0ec2aebfefeb9c40ec80936948719
prashanna-bhattarai <prashanna.bhattarai01@gmail.com>
2026-07-14 11:54:57 +0545

    merge: resolve conflicts with develop branch

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { Eye, EyeOff } from "lucide-react";
 import { signup } from "../stores/authSlice.js";
