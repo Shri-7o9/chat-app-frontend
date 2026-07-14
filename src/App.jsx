783e9430a00bedd6662c890ef7b87a0fbe88d00f
prashanna-bhattarai <prashanna.bhattarai01@gmail.com>
2026-07-14 11:34:12 +0545

    feat: add navbar to app.jsx and missing routes for profile and verify email

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import {  Routes, Route, Navigate } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { checkAuth } from "./stores/authSlice";
-
-import HomePage from "./pages/HomePage"
-import LogInPage from './pages/LogInPage'
-import SignUpPage from './pages/SignUpPage'
+import HomePage from "./pages/HomePage";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
 import UpdateProfilePage from "./pages/updateProfilePage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -23,17 +23,38 @@ const App = () => {
     return <div>Loading...</div>;
   }
 
- return (
+  return (
     <div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={authUser?<HomePage />:<Navigate to="/login" />} />
-        <Route path="/login" element={!authUser?<LogInPage />:<Navigate to="/" />} />
-        <Route path="/signup" element={!authUser?<SignUpPage />:<Navigate to="/" />} />
-        <Route path="/reset-password/:token" element={<ResetPasswordPage/>}/>
-        <Route path="/reset-password" element={<ResetPasswordPage/>}/>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LogInPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route
+          path="/verify-email/:token"
+          element={<div>Verify Email Page</div>}
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <UpdateProfilePage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-
-      <Toaster/>
+      <Toaster />
     </div>
   );
 };
