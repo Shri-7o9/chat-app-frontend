4c41f9c2c931f9a40ada36ae9e8f8e13bd17983a
Shri <shrivatsa709@gmail.com>
2026-07-22 11:06:35 +0545

    final

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -178,12 +178,12 @@ export default function Sidebar({ currentUser, selectedUser }) {
       </div>
 
       {/* Footer profiling current session layout */}
-      <div style={{ paddingTop: "12px", borderTop: "1px solid #333", marginTop: "auto" }}>
+      {/* <div style={{ paddingTop: "12px", borderTop: "1px solid #333", marginTop: "auto" }}>
         <span style={{ fontSize: "14px", color: "#aaa" }}>Logged in as:</span>
         <div style={{ fontWeight: "600", fontSize: "15px", marginTop: "2px" }}>
           {currentUser?.fullName}
         </div>
-      </div>
+      </div> */}
 
       {/* Global query popup modal layout visibility toggler */}
       {showNewChatModal && (
