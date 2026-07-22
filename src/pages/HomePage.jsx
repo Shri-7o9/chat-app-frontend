5ab3603eae4ffd1e5270a6a7d3a796959858eef6
Shri <shrivatsa709@gmail.com>
2026-07-22 13:44:52 +0545

    chaning css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
   return (
     <div style={{ display: "flex", height: "calc(100vh - 60px)" }}>
       {/* Left side — Sidebar fixed width */}
-      <div style={{ width: "300px", flexShrink: 0 }}>
+      <div style={{ width: "600px", flexShrink: 0 }}>
         <MessageRequests />
         <Sidebar
           users={users}
